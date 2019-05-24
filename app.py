0fc3c24c9a72f64670e63e8715db01fcfbde9e90
Lockmaster20 <diogoafernandes20@gmail.com>
2019-05-25 00:58:04 +0100

    Entradas quase completas(alterar tamanho). Adicionado exemplo para outras receitas.

--- app.py
+++ app.py
@@ -6,29 +6,65 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+
+
 @app.route('/entradas_carne')
 def entradas_carne():
     return render_template('entradas/entradas_carne.html')
 
-@app.route('/bcf')
+@app.route('/entradas_carne/bcf')
 def bcf():
     return render_template('entradas/entradas_carne/bcf.html')
 
-@app.route('/mom')
+@app.route('/entradas_carne/mom')
 def mom():
     return render_template('entradas/entradas_carne/mom.html')
 
-@app.route('/cr')
+@app.route('/entradas_carne/cr')
 def cr():
     return render_template('entradas/entradas_carne/cr.html')
 
-@app.route('/em')
-def em():
-    return render_template('em.html')
 
-@app.route('/ev')
-def ev():
-    return render_template('ev.html')
+
+@app.route('/entradas_marisco')
+def entradas_marisco():
+    return render_template('entradas/entradas_marisco.html')
+
+@app.route('/entradas_marisco/sgffa')
+def sgffa():
+    return render_template('entradas/entradas_marisco/sgffa.html')
+
+@app.route('/entradas_marisco/cmc')
+def cmc():
+    return render_template('entradas/entradas_marisco/cmc.html')
+
+
+
+@app.route('/entradas_vegetais')
+def entradas_vegetais():
+    return render_template('entradas/entradas_vegetais.html')
+
+@app.route('/entradas_vegetais/fvg')
+def fvg():
+    return render_template('entradas/entradas_vegetais/fvg.html')
+
+@app.route('/entradas_vegetais/pv')
+def pv():
+    return render_template('entradas/entradas_vegetais/pv.html')
+
+@app.route('/entradas_vegetais/sfbs')
+def sfbs():
+    return render_template('entradas/entradas_vegetais/sfbs.html')
+
+@app.route('/entradas_vegetais/wa')
+def wa():
+    return render_template('entradas/entradas_vegetais/wa.html')
+
+@app.route('/entradas_vegetais/bgb')
+def bgb():
+    return render_template('entradas/entradas_vegetais/bgb.html')
+
+
 
 @app.route('/login')
 def login():
